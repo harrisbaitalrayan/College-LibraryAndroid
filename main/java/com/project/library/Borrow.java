31b705b927cf186b7a577e611d3d7c5355708377
harrisbaitalrayan <harris@baitalrayan.com>
2017-04-13 20:42:14 +0400

    logout

--- main/java/com/project/library/Borrow.java
+++ main/java/com/project/library/Borrow.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -57,6 +58,18 @@ public class Borrow extends AppCompatActivity {
                 } });
 
         }
+
+        Button logout = ((Button)findViewById(R.id.logout));
+        logout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                session.logoutUser();
+
+
+            }
+
+        });
 
 
     }
