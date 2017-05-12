1aad6700bf11386e65fb5e329caa941f9d532a91
harrisbaitalrayan <harris@baitalrayan.com>
2017-05-12 12:10:46 +0400

    action1

--- main/java/com/project/library/MainActivity.java
+++ main/java/com/project/library/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -198,8 +199,13 @@ public class MainActivity extends AppCompatActivity {
 
         //getMenuInflater().inflate(R..main, menu);
 
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionmenu, menu);
 
-        return true;
+        return super.onCreateOptionsMenu(menu);
+
+
+       // return true;
         //return super.onCreateOptionsMenu(menu);
     }
 
